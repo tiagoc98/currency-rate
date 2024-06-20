0d522cf8086fb5009a68e2fa6417afe754bc9e68
Tiago Cardoso <33624471+tpcred@users.noreply.github.com>
2024-06-20 20:44:45 +0100

    Update requirements

--- currency_rate/currency_rate/urls.py
+++ currency_rate/currency_rate/urls.py
@@ -1,22 +1,11 @@
-"""
-URL configuration for currency_rate project.
+from django.urls import path, register_converter
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+from main import views, converters
+
+register_converter(converters.DateConverter, 'date')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('rate/<date:start_date>/<date:end_date>/', views.rate_range, name="rate_range"),
+    path('rate/<date:rate_date>/', views.rate, name="rate_date"),
+    path('rate/', views.rate, name="rate"),
 ]
